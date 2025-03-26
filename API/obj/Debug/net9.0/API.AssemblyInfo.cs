43eff36d58dc6454871022cabeab74c11d3b8d48
Yushna132 <ygobindram@gmail.com>
2025-03-26 11:50:35 +0100

    API connection front end et back end

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9fe58d94c152d1ce50b4dac9b2acc30d64ab228")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4085e0a58cdd36842f7feca0cd6b41ea05d6ce88")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
