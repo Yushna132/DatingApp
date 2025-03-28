7a304d4b2f2fb7993488de5a3589a452e4a2bdbc
Yushna132 <ygobindram@gmail.com>
2025-03-28 19:22:16 +0100

    Working on Routing

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,15 +2,15 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavComponent, HomeComponent],
+  imports: [RouterOutlet,NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
+
 export class AppComponent implements OnInit {
   private accountService = inject(AccountService);
 
